9bc7aea95ffc8c0e5c6471af197e4a86bf9eafb0
John Helder <johnhelderjp@gmail.com>
2024-02-29 22:43:09 -0300

    Criando as funções para abrir e fechar o menu

---
+++ src/script.js
@@ -0,0 +1,12 @@
+const menuMobile = document.getElementById("menu");
+
+function openMenu(){
+ menuMobile.classList.remove("hidden");
+ menuMobile.classList.add("flex");
+}
+
+
+function closeMenu(){
+  menuMobile.classList.remove("flex");
+  menuMobile.classList.add("hidden");
+}
